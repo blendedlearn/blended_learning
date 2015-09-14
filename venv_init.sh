235a7dee614ffc30fc353ef7942f8839345432a7
kaihua.zhou <silegon@gmail.com>
2015-09-14 14:44:39 +0800

    add ubuntu debian package initial enviroment script

---
+++ venv_init.sh
@@ -0,0 +1,5 @@
+sudo apt-get install -y python-setuptools libpq-dev python-dev \
+	python-pip git tig ipython python-setuptools \
+	libmysqlclient-dev mysql-client mysql-server \
+	uwsgi libxml2 libxml2-dev libxslt1-dev python-virtualenv \
+	nginx python-mysqldb
